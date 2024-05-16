7a3855c7c83a99cccaa2806d9fce431bb01bc0fb
AMIRAMOHMED <amiramohmaed887@gmail.com>
2024-05-17 00:13:21 +0300

    .

--- lib/feature/group chat/data/models/message model/message_model.dart
+++ lib/feature/group chat/data/models/message model/message_model.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
 import 'package:mem_admain/feature/group%20chat/data/models/get_all_User.dart';
 
